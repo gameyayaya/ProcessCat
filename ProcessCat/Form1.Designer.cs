e866d8696b7316da282780e9b2753dd1c17e62e2
gameyayaya <30919609+gameyayaya@users.noreply.github.com>
2019-04-18 02:06:07 +0800

    V1.0
    V1.0

---
+++ ProcessCat/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace ProcessCat
+{
+    partial class ProcessCat
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProcessCat));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 27);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(296, 577);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "掃描時間戳記 : ";
+            // 
+            // ProcessCat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 616);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ProcessCat";
+            this.Text = "ProcessCat";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ProcessCat/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace ProcessCat
+{
+    public partial class ProcessCat : Form
+    {
+
+        string[] s ;
+        public ProcessCat()
+        {
+            InitializeComponent();
+            ReadIni();
+            InitTimer();
+        }
+
+        public void InitTimer()
+        {
+            try
+            {
+                timer1 = new Timer();
+                timer1.Tick += new EventHandler(timer1_Tick);
+                timer1.Interval = 3000; // in miliseconds
+                timer1.Start();
+            }
+            catch (Exception err)
+            {
+
+                
+            }
+
+        }
+
+        // 模版【timer trigger】, trigger by timer1
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                label1.Text = "掃描時間戳記 : " + System.DateTime.Now.ToString();
+                CatRun();
+            }
+            catch (Exception err)
+            {
+
+            }
+
+
+        }
+
+        private void CatRun()
+        {
+            s = richTextBox1.Lines.ToArray();
+            foreach (var item in s)
+            {
+                Process[] processes = Process.GetProcessesByName(item.Trim().Replace("\n\r",""));
+                if (processes.Length > 0)
+                {
+                    richTextBox1.Find(item.Trim());
+                    richTextBox1.SelectionColor = Color.Green;
+                }
+                else
+                {
+                    richTextBox1.Find(item.Trim());
+                    richTextBox1.SelectionColor = Color.Red;
+                }
+                richTextBox1.Select(0, 0);
+            }
+           
+        }
+
+        public void ReadIni()
+        {
+            try
+            {
+                string line;
+                System.IO.StreamReader file =
+                    new System.IO.StreamReader(System.IO.Directory.GetCurrentDirectory() + "\\ProcessCat_Config.ini");
+                while ((line = file.ReadLine()) != null)
+                {
+                    richTextBox1.Text += line + "\n";
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+
+
+
+    }
+}
